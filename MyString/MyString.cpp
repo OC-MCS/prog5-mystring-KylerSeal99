e8886c1eb618dfaa7ed8c609928009daa5a70f0a
Kyler Seal <kyler.seal@eagles.oc.edu>
2019-03-01 00:39:40 -0600

    Final Product

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -1,61 +1,114 @@
 // MyString implementation file
+
 #include "MyString.h"
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+//===================================================================
 // Default Constructor
+// Function Definition: assigns a default value to str (char*)
+// Function Parameters: none
+// Return: MyString
 MyString::MyString()
 {
 	str = nullptr;
 }
 
-//Constructor
+//===================================================================
+// Constructor
+// Function Definition: assigns a value to str (char*)
+// Function Parameters: constant char pointer that puts value in what
+// its passed to
+// Return: MyString
 MyString::MyString(const char* s)
 {
 	int size = strlen(s) + 1;
 	str = new char[size];
 	strcpy_s(str, size, s);
 }
-//Destructor
+
+//===================================================================
+// Destructor
+// Function Definition: deletes dynamically allocated memory after a 
+// MyString is created
+// Function Parameters: none
+// Return: MyString
 MyString::~MyString()
 {
+	cout << "In Destructor " << endl;
 	delete[] str;
 }
 
 
+//===================================================================
 // Copy Constructor
+// Function Definition: assigns a value to str (char*)
+// Function Parameters: a MyString
+// Return: MyString
 MyString::MyString(const MyString &other)
 {
-	int size = strlen(other.str) + 1;
-	str = new char[size];
-	strcpy_s(str, size, other.str);
+	cout << "In copy contructor " << endl;
+	if (other.str == nullptr)
+	{
+		str = nullptr;
+	}
+	else
+	{
+		int size = strlen(other.str) + 1;
+		str = new char[size];
+		strcpy_s(str, size, other.str);
+	}
+	
 }
 
+
+//===================================================================
 // Getter Function for Overloaded "==" Operator
+// Function Definition: Gets whatever value is in str for the == 
+// operator and << operator
+// Function Parameters: none
+// Return: pointer to a char 
 char* MyString::c_str() const
 {
 	return str;
 }
 
+
+//===================================================================
 // Overloaded '=' Operator
+// Function Definition: Assigns str to what the function is passed
+// Function Parameters: a MyString
+// Return: a MyString
 MyString MyString::operator = (const MyString &other)
 {
-	int size = strlen(other.str) + 1;
-	if (this != &other)
+	if (other.str == nullptr)
 	{
-		delete[] str;
-		str = new char[size];
-		strcpy_s(str, size, other.str);
+		str = nullptr;
 	}
+	else
+	{
+		int size = strlen(other.str) + 1;
+		if (this != &other)
+		{
+			delete[] str;
+			str = new char[size];
+			strcpy_s(str, size, other.str);
+		}
+	}
+	
 	return *this;
 }
 
+//===================================================================
 // Overloaded '+' Operator
+// Function Definition: Combines str and the passed string
+// Function Parameters: a MyString
+// Return: a MyString
 MyString MyString::operator + (const MyString &other)
 {
 	MyString temp;
-	int size = strlen(other.str) + 1;
+	int size = strlen(other.str) + strlen(str) + 1;
 	temp.str = new char[size];
 	strcpy_s(temp.str, size, str);
 	strcat_s(temp.str, size, other.str);
@@ -63,7 +116,12 @@ MyString MyString::operator + (const MyString &other)
 	return temp;
 }
 
-// Overloaded "<<" Operator
+
+//===================================================================
+// Overloaded <<' Operator
+// Function Definition: couts the passed string
+// Function Parameters: a MyString
+// Return: none
 ostream &operator << (ostream &str, const MyString &other)
 {
 	if (other.c_str() == nullptr)
@@ -78,7 +136,13 @@ ostream &operator << (ostream &str, const MyString &other)
 	return str;
 }
 
-// Overloaded "==" Operator
+
+//===================================================================
+// Overloaded '==' Operator
+// Function Definition: Compares two strings and returns a bool that 
+// tells whether or not they are equal
+// Function Parameters: a MyString
+// Return: a bool
 bool MyString::operator == (const MyString &other)
 {
 	bool isEqual;
--- MyString/testMyString.cpp
+++ MyString/testMyString.cpp
@@ -1,10 +1,19 @@
 // test driver code goes here
+//=====================================================
+// Kyler Seal
+// 3/1/2019
+// Programming Assignment #6
+// Description: MyString
+//=====================================================
 #include <iostream>
 #include "MyString.h"
 using namespace std;
 
+void testFunc(MyString s);
+
 int main()
 {
+
 	cout << "Test Case 1: \n";
 	MyString s1;
 	MyString s2("string2");
@@ -20,12 +29,12 @@ int main()
 	cout << "S2: " << s2 << endl;
 	cout << "S3: " << s3 << endl;
 
-	/*cout << "\n Test Case 3: \n";
+	cout << "\n Test Case 3: \n";
 	s1 = "Hello";
-	s2 = "World";
+	s2 = " World";
 	s3 = s1 + s2;
-	cout << "Assigning s3: " << s3 << " to s1: " << s1 << "+ s2 " << s2 << endl;
-*/
+	cout << "Assigning s3: " << s3 << " to s1: " << s1 << " + s2 " << s2 << endl;
+
 
 	cout << "\n Test Case 4: \n";
 	cout << "Are the strings equal to eachother? (yes) \n"; 
@@ -48,5 +57,32 @@ int main()
 	{
 		cout << "They are not equal \n";
 	}
+
+	cout << "\n Test Case 4: \n";
+	cout << "Can you assign a string to a null ptr? \n";
+	MyString s4;
+	MyString s5 = "string5";
+	cout << "s4: " << s4 << endl;
+	cout << "s5: " << s5 << endl;
+	cout << "s5 = s4 \n";
+	s5 = s4;
+	cout << "s4: " << s4 << endl;
+	cout << "s5: " << s5 << endl;
+
+	cout << "\n Test Case 5: \n";
+	cout << "Pass a MyString by value to a function. (bob) \n";
+	MyString sB("bob");
+	testFunc(sB);
+
 	return 0;
+}
+
+//===================================================================
+// Function Definition: displays if program is in function and also
+// the MyString passed by value to the function
+// Function Parameters: MyString passed by value
+// Return: void
+void testFunc(MyString s)
+{
+	cout << "In test function. sB is " << s << endl;
 }
